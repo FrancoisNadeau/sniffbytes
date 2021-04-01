a64c4afd8c8b20c653458bf841e8c430da883be9
francois <>
2021-04-01 17:21:43 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sniffbytes',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/FrancoisNadeau/sniffbytes.git',
+    license='cc-by-4.0 ',
+    author='francois',
+    author_email='francois.nadeau.1@umontreal.ca',
+    description='Python3 data parsing and cleaning module'
+)
