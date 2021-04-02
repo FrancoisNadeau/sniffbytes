93644a4997cf6012c3ba5f1f98465f7a2668ec24
francois <>
2021-04-01 20:59:46 -0400

    added setup.py

---
+++ description.rst
@@ -0,0 +1,3 @@
+Python3 module featuring sniff_bytes.py, a dialect finder \
+meant to increase the scope of CSV.Sniffer \
+supported input types and avoid crashes
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+#!usr/bin/env/python3
+
+import setuptools
 from setuptools import setup
 
 setup(
-    name='sniffbytes',
-    version='1.0',
-    packages=[''],
-    url='https://github.com/FrancoisNadeau/sniffbytes.git',
-    license='cc-by-4.0 ',
+    name='sniffbytes-francois.nadeau.1',
+    version='0.0.1',
     author='francois',
     author_email='francois.nadeau.1@umontreal.ca',
-    description='Python3 data parsing and cleaning module'
+    description='Python3 data parsing and cleaning module',
+    long_description="Python3 module featuring sniff_bytes.py,a dialect finder meant to increase the scope of CSV.Sniffer's supported input types and avoid crashes",
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    url='https://github.com/FrancoisNadeau/sniffbytes.git',
+    python_requires=">=3.6",
 )
